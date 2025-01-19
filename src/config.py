6d320c4d044232452383774d4c3b3078174c265e
Sushmita <72083459+Sushmita10062002@users.noreply.github.com>
2025-01-19 19:48:40 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ TRAIN_BATCH_SIZE = 32
 VALID_BATCH_SIZE = 16
 EPOCHS = 10
 TRAINING_FILE = "../inputs/ner_dataset.csv"
-MODEL_NAME = "bert-base-cased"
+MODEL_NAME = "bert-base-uncased"
 MODEL_PATH = "../outputs/model.bin"
 TOKENIZER = AutoTokenizer.from_pretrained(
     MODEL_NAME,
